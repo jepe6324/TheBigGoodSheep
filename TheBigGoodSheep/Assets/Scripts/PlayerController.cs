e2ee007b5fd3f20c77460fe994d39e8470f67c9c
Jesper Persson <jepe6324@student.uu.se>
2019-02-04 13:29:52 +0100

    Bug fixed movement code, by making it use fixedupdate(), Implemented Maleks background code.

--- TheBigGoodSheep/Assets/Scripts/PlayerController.cs
+++ TheBigGoodSheep/Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
