7ea399dd72936d70d5050d2f6a48d1313db17403
Jesper Persson <jepe6324@student.uu.se>
2019-02-07 17:09:17 +0100

    Seperated code that didn't need to be in the same script. Made collision between icecream and other objects. The icecream also despawn after a set time.

--- TheBigGoodSheep/Assets/Scripts/Mover.cs
+++ TheBigGoodSheep/Assets/Scripts/Mover.cs
@@ -4,20 +4,13 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour {
 
-	Rigidbody2D myRigidbody;
+	private Rigidbody2D myRigidbody;
+
 	public float speed;
-	public float lifeTime;
 
 	void Start()
 	{
 		myRigidbody = GetComponent<Rigidbody2D>();
 		myRigidbody.velocity = transform.right * speed;
-	}
-
-	void Update()
-	{
-		lifeTime--;
-		if (lifeTime <= 0)
-			Destroy(this.gameObject);
 	}
 }
