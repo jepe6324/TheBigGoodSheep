7d78ada8e9e0f0be0b5b16df42d76b51f9223b64
Jesper Persson <jepe6324@student.uu.se>
2019-02-09 08:27:07 +0100

    Made a ice cube and related scripts. Made a gamemode script that controls when the game goes into gameover, made a gameover scene.

---
+++ TheBigGoodSheep/Assets/Scripts/Gamemode.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gamemode : MonoBehaviour {
+
+	public int iceCreamTimer;
+	public int iceCubeValue;
+
+	private int frames;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		frames++;
+
+		if (frames == 60)
+		{
+			iceCreamTimer--;
+			frames = 0;
+			if (iceCreamTimer == 0)
+			{
+				SceneManager.LoadScene("GameOver");
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log(this.tag + " is colliding with: " + other.tag); // TODO: Remove this debug line
+		if (other.tag == "IceCube") // Checks the other objects tag to see if it is a "IceCube"
+		{ // If it is do the things that it should do when getting into contact with an IceCube
+			Destroy(other.gameObject);
+			iceCreamTimer += iceCubeValue;
+			frames = 0;
+			return;
+		}
+		else
+		{
+			return;
+		}
+	}
+}
