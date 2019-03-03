8c372113d3d6c45ff22c9ccc9fa7a522825d631a
Nicklas <nicklas.svanang@telia.com>
2019-03-04 00:26:02 +0100

    Made Tutorial / Bigger text for score and time / added UI Buttons / scaled scenes

---
+++ TheBigGoodSheep/Assets/Scripts/Change scenes scripts/ChangeTutorial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeTutorial : MonoBehaviour {
+
+    public void Tutorialscene()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+}
