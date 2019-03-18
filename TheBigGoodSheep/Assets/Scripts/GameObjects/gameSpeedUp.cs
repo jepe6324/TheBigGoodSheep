c6d04695e21d3ccb79b3d73c8bf5478461d8800d
Malek Sharabi <m.malek1996@hotamail.com>
2019-03-18 14:12:56 +0100

    Auto stash before merge of "master" and "origin/master"
    Game speed up code

---
+++ TheBigGoodSheep/Assets/Scripts/GameObjects/gameSpeedUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class gameSpeedUp : MonoBehaviour {
+
+
+	private int hiddenScore = ScoreVariables.score;
+
+	void Update () {
+
+		for (int i = 1; i < 10; i++) {
+
+			if (hiddenScore == 1500)
+			{
+				hiddenScore = 0;
+
+				EnemyStats.enemySpeed += 0.1f;
+		}
+	}
+}
+}
