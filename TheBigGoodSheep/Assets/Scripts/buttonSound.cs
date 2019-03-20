747eb9b3ccd108dde0ff93e23a6e56b9265d262c
Jesper Persson <jepe6324@student.uu.se>
2019-03-20 13:25:01 +0100

    ANIMATIONS!!!!!!!!!!!!!!!

---
+++ TheBigGoodSheep/Assets/Scripts/buttonSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonSound : MonoBehaviour
+{
+
+
+    public AudioSource buttonFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void HoverSound()
+    {
+        buttonFx.PlayOneShot(hoverFx);
+    }
+    public void ClickSound()
+    {
+        buttonFx.PlayOneShot(clickFx);
+    }
+}
